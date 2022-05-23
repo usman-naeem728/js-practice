e9c48fe5f80f9dfa048a93751aed2526925ad5d0
JavaScript Projects <76055167+usman-naeem728@users.noreply.github.com>
2022-05-23 11:55:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// Lab 1 =====
+alert("wellcome");
+alert("error! please enter valid password")
+alert("welcome to js land" + "</br>" + "happy coding")
+
+
+// lab 2====
+var userName;
+var myName = "muhammad usman naeem";
+var message;
+message = "hello world"
+alert(message)
+
+alert("pizza" +"</br>" + "pizz" +"</br>"+"piz" +"</br>" + "pi" +"</br>"+"p" );
+
+
+// lab 3 ======
+
+alert("I am 20 years old");
+alert("you have visited 20 times");
+
+document.write("My birth year is 2003"+ "</br>" + "date type of my declared variable is number" + "</br>")
+
+var visitorName = "smai khan";
+var procductTitle = "tshirt";
+var quantity = 2;
+document.write(visitorName + " ordered " + quantity + procductTitle +" from xyz store ")
+
+var variabl1 , variabl2 , variab3;
+
+// legal variable
+// var  num ;
+// var alpha;
+// var myName;
+// var class_name;
+// var class1;
+
+//ilegal variable
+// var 1num;
+// var @Symbol;
+// var _class;
+// var +myName;
+// var apla@_class;
+
+
+// lab 4 ========
